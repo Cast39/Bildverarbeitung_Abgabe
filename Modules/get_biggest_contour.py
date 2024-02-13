d2c3225d63165d5dd5ae3f13aed27a5a7f4d3423
derDosch <philip.dosch@outlook.de>
2024-02-13 13:52:51 +0100

    So JETZT!

--- Modules/get_biggest_contour.py
+++ Modules/get_biggest_contour.py
@@ -10,7 +10,8 @@ def get_biggest_contour(contours):
         # Überprüfe, ob die Fläche größer als die Mindestkonturgröße ist
         if area > biggest_area:
             result_i = i
-    i +=1
+            
+        i +=1
     return contours[result_i]
             
 
