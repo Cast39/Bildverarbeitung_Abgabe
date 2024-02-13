fcd4d85e7b1f30336820d4e9335a73d25873817d
GeneralWuschel <box.ben4@gmail.com>
2024-02-13 12:43:49 +0100

    init routine structure

--- main_routine.py
+++ main_routine.py
@@ -1,5 +1,7 @@
-#getcontours
-#align
-#getcontours
-#crop(conturs)
-#orient
+#get_contours(image) -> contours[]
+#get_Box(conturs, minSize) -> box[], center, angle
+#align(image, center, angle) -> alignedimg
+#getcontours(alignedimg) -> fin_contours[]
+#get_Box(fin_conturs, minSize) -> fin_box[], center, angle
+#crop(alignedimg,fin_box[])
+#orient -TODO
--- test_routine.py
+++ test_routine.py
@@ -1,3 +1,29 @@
+import os
+import cv2
+#own module
+import Modules.get_contours as get_contours
+import Modules.align as align
 import Modules.crop as crop
+import Modules.orient as orient
+import Modules.scale as scale
+
+testdata = {"path":".\Test\in",
+            "destination":".\Test\out",
+            "images":[]}
+
+def main():
+    #init all pictures in /Test/in
+    print(testdata["path"])
+    for file in os.listdir(testdata["path"]):
+        if file.lower().endswith((".jpg",".png",".tiff",".tif")):
+            testdata["images"].append(cv2.imread(testdata["path"]+'\\'+file))
+    print(testdata["images"][0][25,25])
+
+    #test functions
+    for image in testdata["images"]:
+        contours = get_contours(image)
+        print(contours)
 
 
+
+main()
