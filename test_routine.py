8d654a223d6a788756ad51ce4f51c2b037a1c936
GeneralWuschel <box.ben4@gmail.com>
2024-02-14 09:42:11 +0100

    expand

--- test_routine.py
+++ test_routine.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import numpy as np
 
 # own module
 from Modules.get_contours import get_contours
@@ -11,7 +12,7 @@ from Modules.orient import orient
 from Modules.scale import scale
 
 
-testdata = {"path": ".\Test\in", "destination": ".\Test\out", "images": []}
+testdata = {"path": ".\\Test\\in", "destination": ".\\Test\\out", "images": []}
 
 
 def main():
@@ -28,24 +29,39 @@ def main():
         out = image.copy()
         contours = get_contours(image)
         cv2.fillPoly(out, contours, (0, 0, 255))
-        cv2.imshow("Konturen", out)
+        cv2.imshow("out", out)
         cv2.waitKey(0)
         # get_biggest_contour(contours) -> contour
         contour = get_biggest_contour(contours)
         cv2.fillPoly(out, contour, (0, 255, 0))
-        cv2.imshow("Largest Contour", out)
+        cv2.imshow("out", out)
         cv2.waitKey(0)
         # boundarybox(contur, minSize) -> box[], center, angle
         box, center, size, angle = boundary_box(contour)
         cv2.polylines(out, [box], True, color=(255, 0, 0), thickness=4)
-        cv2.imshow("Box", out)
+        cv2.imshow("out", out)
         cv2.waitKey(0)
         # align(image, center, angle) -> alignedimg
         alignedimg = align(image, center, angle, size)
-        cv2.polylines(alignedimg, [box], True, color=(255, 0, 0), thickness=4)
-        cv2.imshow("Alignement", alignedimg)
+        out = alignedimg.copy()
+        out = cv2.polylines(out, [box], True, color=(255, 0, 0), thickness=4)
+        cv2.imshow("out", out)
+        cv2.waitKey(0)
+        # crop(alignedimg,fin_box[])
+        out = crop(
+            alignedimg, boundary_box(get_biggest_contour(get_contours(alignedimg)))[0]
+        )
+        print(np.flip(out.shape[:2]))
+        cv2.imshow("out", out)
+        cv2.resizeWindow("out", np.flip(image.shape[:2]))
+        cv2.waitKey(0)
+        # orient(alignedimg) -> orientedimg
+        out = orient(out)
+        cv2.imshow("out", out)
         cv2.waitKey(0)
 
+
+        cv2.waitKey(0)
         cv2.destroyAllWindows()
 
 
