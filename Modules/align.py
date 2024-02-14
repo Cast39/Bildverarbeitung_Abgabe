83ae34ad17b7a88f3124305efeacd9ecc0b9131a
GeneralWuschel <box.ben4@gmail.com>
2024-02-14 08:40:38 +0100

    rm debug output

--- Modules/align.py
+++ Modules/align.py
@@ -1,16 +1,17 @@
 import numpy as np
 import cv2
 
-def align(image,center,angle, size):
 
-    if(angle > 45):
+def align(image, center, angle, size):
+
+    if angle > 45:
         angle -= 90
-        print("angle is bigger 45")
+        # DEBUG: print("angle is bigger 45")
 
- 
- 
     alignImage = image.copy()
     # Rotiere das Bild um den Winkel
     align_matrix = cv2.getRotationMatrix2D(center, angle, scale=1)
-    alignImage = cv2.warpAffine(alignImage, align_matrix, (alignImage.shape[1], alignImage.shape[0]))
+    alignImage = cv2.warpAffine(
+        alignImage, align_matrix, (alignImage.shape[1], alignImage.shape[0])
+    )
     return alignImage
