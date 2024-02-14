d0dcb054888ec7db94cb0da14c2b525e5fde5ba4
GeneralWuschel <box.ben4@gmail.com>
2024-02-14 10:08:05 +0100

    added TODO

--- Modules/save_to_png.py
+++ Modules/save_to_png.py
@@ -3,6 +3,7 @@ import cv2
 import os
 
 def saveToPNG(image,folder_path,file_name):
+    # TODO: OS-Handling (try, catch) TODO:Return value (maybe new filepath?)
     output_file_name = os.path.splitext(file_name)[0] + "_edited.png"
     output_path = os.path.join(folder_path, output_file_name)
     cv2.imwrite(output_path, image)
