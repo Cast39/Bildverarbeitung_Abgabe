f56ecaec0e8cbb9f4093cc9a2c9a8cb6fa9fef2b
Benjamin Bogs <box.ben4@gmail.com>
2024-02-16 01:08:55 +0100

    .PNG compliance

--- Modules/get_contours.py
+++ Modules/get_contours.py
@@ -4,7 +4,7 @@ import cv2
 
 def get_contours(image):
     # Berechne das umschließende Rechteck nach der Rotation
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(image, cv2.COLOR_RGBA2GRAY)
     #gray = cv2.bitwise_not(gray)
     contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
--- Modules/save_to_png.py
+++ Modules/save_to_png.py
@@ -2,16 +2,20 @@ import cv2
 import os
 
 
-def saveToPNG(image, folder_path, file_name, file_name_appendix="_edited"):
+def saveToPNG(image, folder_path, file_name, file_name_appendix="edited", overwrite=True):
     # Synthezise file path
     output_file_name = (
         os.path.splitext(file_name)[0] + "_" + file_name_appendix + ".png"
     )
     output_path = os.path.join(folder_path, output_file_name)
     # try writing it
-    if not (os.path.exists(folder_path)):
+    if not (os.path.exists(folder_path)):  # unless folder doesn't exist
         return (-1, "path dosn't exist")
+    if (os.path.exists(output_path) and overwrite == False):  # unless filename exists
+        return (-1, "file exists already")
+
     try:
+        cv2.cvtColor(image, cv2.COLOR_RGBA2BGRA)
         cv2.imwrite(output_path, image)
         return (0, output_path)
     except cv2.error:
