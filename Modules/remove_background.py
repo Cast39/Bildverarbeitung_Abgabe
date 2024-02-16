df36291e3a40e06341df6133f8a788992d71dedf
Benjamin Bogs <box.ben4@gmail.com>
2024-02-16 01:12:23 +0100

    [SYNC] WIP

--- Modules/remove_background.py
+++ Modules/remove_background.py
@@ -3,14 +3,8 @@ import cv2
 
 
 def remove_background(image, colour=(5, 5, 5), tolerance=15):
-    param = cv2.SimpleBlobDetector.Params()
-    param.filterByColor = True
-    param.blobColor = 0
-    param.minThreshold = 10
-    param.maxThreshold = 150
-
-    detector = cv2.SimpleBlobDetector()
-    detector= detector.create(param)
-    blobs = detector.detect(image)
-    cv2.drawKeypoints(image, blobs, image, (0, 0, 0, 0))
+    subject = np.float32(image)
+    _,label,center = cv2.kmeans(subject,2,None,(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0),10,cv2.KMEANS_RANDOM_CENTERS)
+    center = np.uint8(center)
+    image = center[label.flatten()].reshape((image.shape))
     return image
